f05a995b38f61d762b72dfe2adb4a50a8842a9c2
takumi-shibata <241448ta@gmail.com>
2020-08-27 00:54:44 +0900

    １周目学習終了

--- index.rb
+++ index.rb
@@ -141,3 +141,16 @@ if score >= 95 && score <= 99
   puts "高得点です！次は満点を目指しましょう。"
 end
 # →高得点です！次は満点を目指しましょう。
+
+
+number = 48
+if number % 3 == 0 && number % 5 == 0
+  puts "15の倍数です"
+elsif number % 5 == 0
+  puts "5の倍数です"
+elsif number % 3 == 0
+  puts "3の倍数です"
+else
+  puts "3の倍数でも5の倍数でもありません"
+end
+# →3の倍数です
