02e34858ec1f5b5b0fd4805b5f624f609465653e
takumi-shibata <241448ta@gmail.com>
2020-09-16 23:52:36 +0900

    2周目 変数展開まで終了

--- index.rb
+++ index.rb
@@ -33,3 +33,59 @@ puts "私は" + "にんじゃわんこです"
 
 puts "38" + "19"
 # →3819
+
+
+
+# 4.変数(2語以上の場合、Rubyだと「_」アンダーバーで区切る。jsは小文字と大文字)
+name = "にんじゃわんこ"
+puts name
+# →にんじゃわんこ
+
+text = "プログラミングを学ぼう"
+puts "Progateで" + text
+# →Progateでプログラミングを学ぼう
+
+length = 8
+width = 9
+puts length * width
+# →72
+
+
+
+# 5.変数の更新
+length = 9
+width = 8
+puts width
+puts length * width
+# →8
+# →72
+
+width = 13 #(width = 8から、width = 13に更新)
+puts width
+puts length * width
+# →13
+# →117
+
+length = 9
+width = 8
+puts width
+puts length * width
+# →8
+# →72
+
+width += 5  #(width = width + 5の省略形、他の計算も同様)
+puts width
+puts length * width
+# →13
+# →117
+
+
+
+# 6.変数展開(jsでいうテンプレートリテラル、「＋」記号を用いらなくとも文字や数字を連結させる事が出来る方法。${}か#{}の違いだけ)
+# 変数展開の際は必ず「""」ダブルコーテーションで囲う!「''」シングルコーテーションはNG!
+# ※通常、数値の入った変数と文字列を足し算で連結することはできないが、変数展開を用いると数値の入った変数も問題なく文字列に含めることが可能
+length = 9
+width = 8
+area = length * width
+puts "面積は#{area}です"
+# →面積は72です
